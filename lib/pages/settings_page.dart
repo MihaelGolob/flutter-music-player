3c06fa31c8168068bdb5f178fe6e74269ba6af81
mihael.golob <golob.mihael@gmail.com>
2024-07-24 22:31:35 +0200

    add dark mode switch to the settings

--- lib/pages/settings_page.dart
+++ lib/pages/settings_page.dart
@@ -1,15 +1,42 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_music_player/components/my_app_bar.dart';
+import 'package:flutter_music_player/themes/theme_provider.dart';
+import 'package:provider/provider.dart';
 
 class SettingsPage extends StatelessWidget {
   const SettingsPage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final themeProvider = Provider.of<ThemeProvider>(context);
+
     return Scaffold(
       backgroundColor: Theme.of(context).colorScheme.surface,
-      appBar: MyAppBar(
+      appBar: const MyAppBar(
         title: 'Settings',
+      ),
+      body: Column(
+        children: [
+          Container(
+            margin: const EdgeInsets.all(25),
+            padding: const EdgeInsets.all(15),
+            decoration: BoxDecoration(
+              color: Theme.of(context).colorScheme.secondary,
+              borderRadius: BorderRadius.circular(15),
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                const Text(
+                  'Dark Mode',
+                  style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
+                ),
+                CupertinoSwitch(value: themeProvider.isDarkMode, onChanged: (value) => themeProvider.toggleTheme()),
+              ],
+            ),
+          )
+        ],
       ),
     );
   }
--- lib/themes/dark_mode.dart
+++ lib/themes/dark_mode.dart
@@ -4,7 +4,7 @@ ThemeData darkMode = ThemeData(
   colorScheme: ColorScheme.dark(
     surface: Colors.grey.shade900,
     primary: Colors.grey.shade600,
-    secondary: Colors.grey.shade800,
+    secondary: const Color.fromARGB(255, 41, 41, 41),
     inversePrimary: Colors.grey.shade300,
   ),
 );
